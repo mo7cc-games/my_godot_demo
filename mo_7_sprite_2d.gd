7e2bb787ef22b34eb9c4d15fca033a0feb7e3f25
墨七 <meichangliang@outlook.com>
2025-04-08 01:23:20 +0800

    玩家输入

--- mo_7_sprite_2d.gd
+++
@@ -1,9 +0,0 @@
-extends Sprite2D
-
-var speed = 400
-var angular_speed = PI
-
-func _process(delta):
-	rotation += angular_speed * delta
-	var velocity = Vector2.UP.rotated(rotation) * speed
-	position += velocity * delta
