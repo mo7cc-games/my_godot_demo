aef6895a0da192580580eba198f00811a4d8bc46
墨七 <meichangliang@outlook.com>
2025-04-08 02:28:49 +0800

    x

--- node_2d/first_input/first_input.gd
+++ node_2d/first_input/first_input.gd
@@ -2,24 +2,28 @@ extends Sprite2D
 
 var speed = 400
 var angular_speed = PI
-var mo7_xx = 123
+
+# func _process(delta):
+# 	# rotation += angular_speed * delta
+# 	# var velocity = Vector2.UP.rotated(rotation) * speed
+# 	# position += velocity * delta
+# 	var direction = 0
+# 	if Input.is_action_pressed("ui_left"):
+# 		direction = -1
+# 	if Input.is_action_pressed("ui_right"):
+# 		direction = 1
+# 	rotation += angular_speed * direction * delta
+# 	var velocity = Vector2.ZERO
+# 	if Input.is_action_pressed("ui_up"):
+# 		velocity = Vector2.UP.rotated(rotation) * speed
+# 	position += velocity * delta
+
 
 func _process(delta):
-	# rotation += angular_speed * delta
-	# var velocity = Vector2.UP.rotated(rotation) * speed
-	# position += velocity * delta
-	var direction = 0
-	if Input.is_action_pressed("ui_left"):
-		direction = -1
-	if Input.is_action_pressed("ui_right"):
-		direction = 1
-	rotation += angular_speed * direction * delta
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("ui_up"):
-		velocity = Vector2.UP.rotated(rotation) * speed
+	rotation += angular_speed * delta
+	var velocity = Vector2.UP.rotated(rotation) * speed
 	position += velocity * delta
 
-
 func _on_button_pressed() -> void:
-	print("Button pressed", mo7_xx)
-	pass # Replace with function body.
+	print("Button:", is_processing())
+	set_process(not is_processing()) # 暂停帧处理
